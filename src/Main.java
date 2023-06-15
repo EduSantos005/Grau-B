1dfa61f80df5d846c6644731ace24beaa2276d96
Eduardo dos Santos <EduardoSantos10@edu.unisinos.br>
2023-06-14 21:36:56 -0300

    Preços Ajustados

--- src/Main.java
+++ src/Main.java
@@ -80,9 +80,9 @@ public class Main {
 		Produto pp = new Livro("Pequeno Príncipe", 40, 201, 2, "Antoine de Saint-Exupéry", "Ficção");
 		Produto crepusculo = new Livro("Crepúsculo", 50, 202, 5, "Stephenie Meyer", "Romance");
 		Produto dk = new Livro("Dom Quixote", 30, 203, 3, "Miguel de Cervantes", "Aventura");
-		Produto PC = new Eletrônico("PC Gamer", 33000, 301, 3, "AMD Ryzen 5 4600G, 16GB DDR4, SSD 480GB, Monitor 21'");
+		Produto PC = new Eletrônico("PC Gamer", 3300, 301, 3, "AMD Ryzen 5 4600G, 16GB DDR4, SSD 480GB, Monitor 21'");
 		Produto iphone = new Eletrônico("Iphone 14", 8000, 302, 10, "Apple iPhone14 Pro Max 128 GB");
-		Produto monitor = new Eletrônico("Monitor 23,5'", 800, 303, 5, "Samsung");
+		Produto monitor = new Eletrônico("Monitor 23,5'", 1400, 303, 5, "Samsung");
 		Produto freezer = new Eletrodoméstico("Freezer", 3500, 401, 4, "Horizontal Consul 2 portas");
 		Produto geladeira = new Eletrodoméstico("Geladeira", 2800, 402, 8, "Electrolux");
 		Produto fogao = new Eletrodoméstico("Fogão", 2000, 403, 8, "Brastemp 4 bocas");
